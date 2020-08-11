277e52ab855568256781fc717df8f51494083cfe
iness18 <47697936+iness18@users.noreply.github.com>
2020-08-11 23:28:50 +0200

    wip

--- assets/js/main.js
+++ assets/js/main.js
@@ -102,4 +102,16 @@
 			usePopupNav: true
 		});
 
+
+		// Closes responsive menu when a scroll trigger link is clicked
+	$(".js-scroll-trigger").click(function () {
+			$(".navbar-collapse").collapse("hide");
+	});
+
+	// Activate scrollspy to add active class to navbar items on scroll
+	$("body").scrollspy({
+			target: "#mainNav",
+			offset: 100,
+	});
+
 })(jQuery);
